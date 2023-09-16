d990d686dba29d979382946804aa065c3f8215a9
FREDY ANTONIO MAZO QUICENO <frema1914@gmail.com>
2023-09-16 13:39:23 -0500

    Class Date.cs created and dateObject built in Program.cs

---
+++ EmployeeOOP/Clases/Date.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeOPP.Clases
+{
+    public class Date
+    {
+        #region Fields
+        //atributos
+        private int _year;
+        private int _month;
+        private int _day;
+
+        #endregion
+
+        #region Methods
+        //Método constructor
+        public Date(int year, int month, int day)
+        {
+            //se inicializan los campos
+            _year = ValidateYear(year);
+            _month = ValidateMonth(month);//Se llama el método ValidateMonth y se le envía month
+            _day = ValidateDay(day, month, year);
+        }
+
+        private int ValidateDay(int day, int month, int year)
+        {
+            return 2;
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            bool IsLeapYear = year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+            return IsLeapYear;
+        }
+
+        private int ValidateYear(int year)
+        {
+            if (year >= 1900)
+            {
+                return year;
+            }
+            else
+            {
+                throw new YearException(String.Format("El año {0} no es valido!", year));
+            }
+        }
+
+        private int ValidateMonth(int month)
+        {
+            if (month >= 1 && month <= 12)//Falso
+            {
+                return month;
+            }
+            else
+            {
+                throw new MonthException(String.Format("El mes {0} no es valido!", month));
+            }
+        }
+
+        //Se llama el método ToString pero con override se le da un comportamiento diferente
+        public override string ToString()
+        {
+            //Tres formas de concatenar
+            var dateConcatenated1 = _day + "/" + _month + "/" + _year;
+            var dateConcatenated2 = ($"{_day:00}/{_month:00}/{_year:0000}"); //Interpolación
+            var dateConcatenated3 = String.Format("{0:00}/{1:00}/{2:0000}", _day, _month, _year);//Clase String.Format()
+            return dateConcatenated3;
+        }
+
+        #endregion
+    }
+}
---
+++ EmployeeOOP/Program.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeOPP.Clases;
+using System;
+
+namespace EmployeeOPP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Controlar exepciones
+            try
+            {
+                Console.WriteLine("OOP APPLICATION");
+                Console.WriteLine("---------------");
+
+                //Declarar algunas variables
+                int day, month, year;
+
+                Console.Write("Ingresar el día: ");
+                day = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Ingresar el mes: ");
+                //Todo lo que entra a ReadLine(), llega como String, para otro tipo de dato se debe hacer conversión
+                month = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Ingresar el año: ");
+                year = Convert.ToInt32(Console.ReadLine());
+
+                //Instaciar la clase Date (crear el objeto)
+                Date dateObject = new Date(year, month, day);
+                Console.Write("\n");//Salto de línea
+                Console.WriteLine(dateObject.ToString());
+            }
+            catch (Exception ex)//ex es una variable
+            {
+                Console.Write("\n");
+                Console.WriteLine(ex.Message);//Pintamos el mensaje
+            }
+        }
+    }
+}
